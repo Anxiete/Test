79e2c569c7e62d47c66b8fdbddefa67f602cb6dd
Isaac <rodcel.isaac18@gmail.com>
2022-12-01 11:54:34 -0600

    Fixed the issue where bucket is not being found and images not being display properly.

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,7 @@
 class Card < ApplicationRecord
   has_many :comments
-  belongs_to :card_clan
+  belongs_to :card_clan 
   has_one_attached :image
-  validates :name, :power, :shield, :grade, :race, :effect, :format, presence: true
+  validates :name, :power, :shield, :grade, :race, :effect, :format, :price, presence: true
+  validates :card_clan_id, :price, numericality: true
 end
