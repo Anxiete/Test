a50da697225aaf4355bc4d4a0843185da1f6dcda
Isaac <rodcel.isaac18@gmail.com>
2022-12-20 15:36:19 -0600

    Fixed search

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -9,22 +9,18 @@ class CardController < ApplicationController
 
   def search
     @search_header = true
-    search = params[:search].to_s
+    search = params[:search]
     clan = params[:clans].to_s
     grade = params[:grades].to_s
 
     @cards_search = if params[:grades].present? && params[:clans].present?
-                      Card.where("name LIKE ? AND card_clan_id = ? AND grade = ?", "%#{search}%", clan.to_s,
-                                 grade.to_s).order(:name).includes(:card_clan).all.page(params[:page]).per(12)
+                      Card.where('name LIKE ? AND card_clan_id = ? AND grade = ?', "%#{search}%", clan.to_s, grade.to_s).order(:name).includes(:card_clan).all.page(params[:page]).per(12)
                     elsif params[:grades].present?
-                      Card.where("name LIKE ? AND grade = ?", "%#{search}%",
-                                 grade.to_s).order(:name).includes(:card_clan).all.page(params[:page]).per(12)
+                      Card.where('name LIKE ? AND grade = ?', "%#{search}%", grade.to_s).order(:name).includes(:card_clan).all.page(params[:page]).per(12)
                     elsif params[:clans].present?
-                      Card.where("name LIKE ? AND card_clan_id = ?", "%#{search}%",
-                                 clan.to_s).order(:name).includes(:card_clan).all.page(params[:page]).per(12)
+                      Card.where('name LIKE ? AND card_clan_id = ?', "%#{search}%", clan.to_s).order(:name).includes(:card_clan).all.page(params[:page]).per(12)
                     else
-                      Card.where("name LIKE ?",
-                                 "%#{search}%").order(:name).includes(:card_clan).all.page(params[:page]).per(12)
+                      Card.where('name LIKE ?', "%#{search}%").order(:name).includes(:card_clan).all.page(params[:page]).per(12)
                     end
   end
 end
