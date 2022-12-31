e1bfdb20606c0ed1b2e4a83f02877eb7735d9ec7
Isaac <rodcel.isaac18@gmail.com>
2022-12-31 08:59:05 -0600

    Fix orders page for heroku.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   def index
-    @orders = Order.where("user_id LIKE ?", current_user.id)
+    @orders = Order.where("user_id LIKE ?", current_user.id.to_s)
   end
 
   def create
